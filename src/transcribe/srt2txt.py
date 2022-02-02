29fddb44be530afda33b0be8565c1ebb4c0fb939
Wilson Wongso <wilsonwong961@gmail.com>
2022-02-02 13:18:30 +0700

    fixed srt read error

--- src/transcribe/srt2txt.py
+++ src/transcribe/srt2txt.py
@@ -16,9 +16,10 @@ def srt2txt(srt_string):
     """
     subs = pysrt.from_string(srt_string)
     texts = [sub.text for sub in subs]
+    # filter for empty strings
+    texts = list(filter(lambda text: len(text) > 0, texts))
     # filter special tokens like [Music] and [Applause]
     texts = list(filter(lambda text: text[0] != "[" and text[-1] != "]", texts))
     texts = " ".join(texts)
     texts = texts.replace("\n", " ")
     return texts
-
