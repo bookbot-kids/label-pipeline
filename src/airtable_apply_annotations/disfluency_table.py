61defaa7df62789bc0dc3284869d6ced9e5534af
Wilson Wongso <wilsonwong961@gmail.com>
2022-02-14 16:32:19 +0700

    re-designed disfluency pipeline to be uniform with other tables

--- src/airtable_apply_annotations/disfluency_table.py
+++ src/airtable_apply_annotations/disfluency_table.py
@@ -1,6 +1,6 @@
 from airtable_s3_integration import AirTableS3Integration
 from typing import Dict, Any, List
-from s3_utils import copy_file, delete_file, move_file, bulk_s3_actions
+from s3_utils import delete_file, move_file, write_file
 import json
 
 
@@ -28,7 +28,7 @@ class DisfluencyTable(AirTableS3Integration):
     _apply_annotation_changes_s3(record: Dict[str, Any]) -> None:
         Applies changes in an S3 directory based on an AirTable `record`'s disfluency verdict.
     _finalize_record(record: Dict[str, Any]) -> None:
-        Finalizes a disfluency record by marking "AWS" column as `True` and updating latest disfluency.
+        Finalizes a disfluency record by marking "AWS" column as `True`.
     """
 
     def __init__(self, airtable_url: str, filter_formula: str, headers: Dict[str, str]):
@@ -52,79 +52,31 @@ class DisfluencyTable(AirTableS3Integration):
         ----------
         record : Dict[str, Any]
             An AirTable record/row.
-
-        Examples
-        -----------------------------------------------------
-        | Disfluency | Actual Disfluency |      Verdict     |
-        |:----------:|:-----------------:|:----------------:|
-        |      Σ     |         -         |    Do Nothing    |
-        |      Σ     |        None       |   Delete from Σ  |
-        |      A     |         B         | Move from A to B |
-        |      A     |        A, B       | Copy from A to B |
-        |    A, B    |         A         |   Delete from B  |
-        -----------------------------------------------------
         """
-
-        def _get_files_with_extensions(
-            job_name: str, extensions: List[str]
-        ) -> List[str]:
-            return [f"{job_name}.{extension}" for extension in extensions]
-
-        def _lowercase_elements(list_):
-            return list(map(lambda x: x.lower(), list_))
-
-        def _get_directories(disfluencies: List[str], language: str) -> List[str]:
-            return [
-                f"mispronunciations/{disfluency}/{language}"
-                for disfluency in disfluencies
-            ]
-
-        def _list_difference(list1: List, list2: List) -> List:
-            assert len(list1) > len(list2)
-            return list(set(list1) - set(list2))
-
         fields = record["fields"]
 
-        # no changes necessary
-        if "Actual Disfluency" not in fields:
-            return
-
         job_name, language = fields["Job Name"], fields["Language"]
-        files = _get_files_with_extensions(job_name, self.extensions)
+        disfluency, transcript = fields["Disfluency"].lower(), fields["Transcript"]
+        audio_filename = fields["Audio"][0]["filename"]
 
-        disfluency = _lowercase_elements(fields["Disfluency"])
-        actual_disfluency = _lowercase_elements(fields["Actual Disfluency"])
+        source_path = f"mispronunciations/raw/{language}"
+        save_path = f"mispronunciations/{disfluency}/{language}"
 
-        sources = _get_directories(disfluency, language)
-        targets = _get_directories(actual_disfluency, language)
-
-        if actual_disfluency == ["none"]:  # delete all copies
-            bulk_s3_actions(delete_file, self.bucket, files, sources)
-        elif len(disfluency) == 1 and len(actual_disfluency) == 1:  # move from A to B
-            bulk_s3_actions(move_file, self.bucket, files, sources, targets)
-        elif (
-            len(disfluency) == 1 and len(actual_disfluency) == 2
-        ):  # make additional copy
-            actual_disfluency = _list_difference(actual_disfluency, disfluency)
-            targets = _get_directories(actual_disfluency, language)
-            bulk_s3_actions(copy_file, self.bucket, files, sources, targets)
-        elif len(disfluency) == 2 and len(actual_disfluency) == 1:  # delete additional
-            disfluency = _list_difference(disfluency, actual_disfluency)
-            sources = _get_directories(disfluency, language)
-            bulk_s3_actions(delete_file, self.bucket, files, sources)
+        if disfluency == "delete":
+            delete_file(self.bucket, audio_filename, source_path)
+        else:
+            move_file(self.bucket, audio_filename, source_path, save_path)
+            write_file(self.bucket, transcript, save_path, f"{job_name}.txt")
 
     def _finalize_record(self, record: Dict[str, Any]):
         """Finalizes a disfluency record by marking "AWS" column as `True`.
-        If "Actual Disfluency" is empty (because "Disfluency" is correctly predicted), copy the latter's value to the former.
 
         Parameters
         ----------
         record : Dict[str, Any]
             An AirTable record.
         """
-        fields = {"AWS": True}
-        if "Actual Disfluency" not in record["fields"]:
-            fields["Actual Disfluency"] = record["fields"]["Disfluency"]
-
-        payload = json.dumps({"records": [{"id": record["id"], "fields": fields}]})
+        payload = json.dumps(
+            {"records": [{"id": record["id"], "fields": {"AWS": True}}]}
+        )
         return payload
