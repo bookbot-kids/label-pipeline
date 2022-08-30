3c037148aa692def5bed85a39650ddfb52b5a38d
w11wo <wilsonwong961@gmail.com>
2022-08-30 10:25:37 +1000

    integrated moto for AWS tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,19 @@
+import os
 import pytest
 import boto3
+from moto import mock_transcribe
 
 
-@pytest.fixture(scope="session")
-def transcribe_client():
-    return boto3.client("transcribe")
+@pytest.fixture
+def aws_credentials():
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+
+
+@pytest.fixture()
+def transcribe_client(aws_credentials):
+    with mock_transcribe():
+        conn = boto3.client("transcribe", region_name="ap-southeast-1")
+        yield conn
